97dfb9f44b5273e935d5490a5bbbb2993f752a1a
Morgan Vickery <morganavickery@gmail.com>
2019-01-31 17:52:35 +0000

    quiz submission

---
+++ main.hs
@@ -0,0 +1,7 @@
+sumAll :: [Int] -> Int
+sumAll [] = 0
+sumAll (x:xs) = x + sumAll(xs)
+
+finalGrade :: [Int] -> [Int] -> Int
+finalGrade [] [] = 0
+finalGrade x:xs y:ys = (x * (y `div` sumAll (y:ys))) + (finalGrade xs ys)
