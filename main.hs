97a9fd200bd4ef28992931bb3f51adbc730cbd74
Morgan Vickery <morganavickery@gmail.com>
2019-01-31 18:12:34 +0000

    last shot

--- main.hs
+++ main.hs
@@ -3,5 +3,6 @@ sumAll [] = 0
 sumAll (x:xs) = x + sumAll(xs)
 
 finalGrade :: [Int] -> [Int] -> Int
+finalGrade [] [] = 0
 finalGrade (x:xs) (y:ys) = preDiv `div` (sumAll(ys))
   where preDiv = ((x * y) + (finalGrade(xs) (ys)))
